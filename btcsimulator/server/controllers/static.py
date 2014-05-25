127cff36601c72a22f61e4f22bf025185f375901
Victor <tagoro9@gmail.com>
2014-05-25 19:14:54 +0100

    add pushstate support

--- btcsimulator/server/controllers/static.py
+++ btcsimulator/server/controllers/static.py
@@ -5,3 +5,7 @@ from . import app
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+@app.errorhandler(404)
+def push_state(e):
+    return app.send_static_file('index.html')
